f5e5fb38c1a3d9fc5e72067e6937060b919b50a8
Paul Pistorius <jpistoriusp@gmail.com>
2017-06-26 08:41:15 -0600

    working delete

--- FuelTrackerRest/WebContent/app.js
+++ FuelTrackerRest/WebContent/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	start();
 });
 var currentMiles = 0;
+var totalPrice = 0;
 
 var start = function() {
 	var myReq = $.ajax({
@@ -36,29 +37,54 @@ var buildList = function(data) {
 		var td2 = $('<td>').text(entry.endMiles);
 		var td3 = $('<td>').text(entry.gallons);
 		var td4 = $('<td>').text(entry.price);
-		var deleteButton = $('<button>').text('Delete').attr('deleteid', entry.id);
-		var editButton = $('<button>').text('Edit').attr('editid', entry.id);
+		var deleteButton = $('<button>').text('Delete').attr('id', entry.id);
+		var editButton = $('<button>').text('Edit').attr('id', entry.id);
 		deleteButton.on('click', deleteButtonFunction);
-		editButton.on('click', loadFillUpEditForm);
-		tr.append(td1, td2, td3, td4, deleteButton, editButton);
+		editButton.on('click', loadFillUpEditForm());
+		tr.append(td1, td2, td3, td4, editButton, deleteButton);
 		tbody.append(tr);
+		console.log(entry.price);
 	});
 	var newEntryButton = $('<button>').text('New Entry');
 	newEntryButton.on('click', loadFillUpEntryForm);
 	$('#table').append(newEntryButton);
 	table.append(tbody);
 	$('#table').append(table);
+	$('#totals').append("Total Amount Spent: " + totalPrice);
 }
 
 var loadFillUpEntryForm = function() {
-	$('#table').load('Form.html', function(){
+	$('#table').load('Form.html', function() {
 		$(fillUpEntry.submit).on('click', entryForm);
 	});
 }
 
 var loadFillUpEditForm = function() {
-	$('#table').load('Form.html', function(){
-		$(fillUpEntry.submit).on('click', editForm);
+	$('#table').load('editForm.html', function() {
+		$(fillUpEdit.submit).on('click', function(e) {
+			e.preventDefault();
+			var obj = {
+				gallons : $(fillUpEntry.gallons).val(),
+				price : $(fillUpEntry.price).val(),
+				startMiles : currentMiles,
+				endMiles : $(fillUpEntry.endMiles).val()
+			}
+			console.log($(e).attr('id'))
+			var myReq = $.ajax({
+				type : "POST",
+				url : "rest/fueltracker/ " + $(e).attr('id'),
+				dataType : "json",
+				contentType : 'application/json',
+				data : JSON.stringify(obj)
+			});
+			myReq.done(function(data, status) {
+				$('#table').empty();
+				start();
+			});
+			myReq.fail(function(xhr, status, error) {
+				console.log('It blew up again');
+			});
+		});
 	});
 }
 
@@ -69,14 +95,15 @@ var deleteButtonFunction = function() {
 	});
 	myReq.done(function(data, status) {
 		$('#table').empty();
+		$('#totals').empty();
 		start();
 	});
 	myReq.fail(function(xhr, status, error) {
 		console.log(error);
 	});
-});
+};
 
-var entryForm = function(e){
+var entryForm = function(e) {
 	e.preventDefault();
 	var obj = {
 		gallons : $(fillUpEntry.gallons).val(),
@@ -89,39 +116,16 @@ var entryForm = function(e){
 		type : "POST",
 		url : "rest/fueltracker",
 		dataType : "json",
-		contentType: 'application/json',
-		data: JSON.stringify(obj)
+		contentType : 'application/json',
+		data : JSON.stringify(obj)
 	});
 	myReq.done(function(data, status) {
+		totalPrice = totalPrice + parseInt(data.price);
 		$('#table').empty();
+		$('#totals').empty();
 		start();
 	});
 	myReq.fail(function(xhr, status, error) {
 		console.log('It blew up again');
 	});
-});
-
-var editForm = function(e){
-	e.preventDefault();
-	var obj = {
-		gallons : $(fillUpEntry.gallons).val(),
-		price : $(fillUpEntry.price).val(),
-		startMiles : currentMiles,
-		endMiles : $(fillUpEntry.endMiles).val()
-	}
-	currentMiles = $(fillUpEntry.endMiles).val();
-	var myReq = $.ajax({
-		type : "POST",
-		url : "rest/fueltracker",
-		dataType : "json",
-		contentType: 'application/json',
-		data: JSON.stringify(obj)
-	});
-	myReq.done(function(data, status) {
-		$('#table').empty();
-		start();
-	});
-	myReq.fail(function(xhr, status, error) {
-		console.log('It blew up again');
-	});
-});
+};
